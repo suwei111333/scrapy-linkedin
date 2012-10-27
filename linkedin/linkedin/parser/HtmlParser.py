e804e7f31ac3bcaf70632d383fb6dfe0be330f54
Wei Chen <cleverdie@gmail.com>
2012-10-27 16:05:56 +0800

    can parse name & also view

--- linkedin/linkedin/parser/HtmlParser.py
+++ linkedin/linkedin/parser/HtmlParser.py
@@ -6,8 +6,22 @@ class HtmlParser:
     @staticmethod
     def extract_person_profile(hxs):
         personProfile = PersonProfileItem()
-        personProfile['name'] = {"first_name":"wei chen", "last_name":"chen"}
-        
+        ## Person name
+        nameField = {}
+        nameSpan = hxs.select("//span[@id='name']/span")
+        if nameSpan and len(nameSpan) == 1:
+            nameSpan = nameSpan[0]
+            givenNameSpan = nameSpan.select("span[@class='given-name']")
+            if givenNameSpan and len(givenNameSpan) == 1:
+                givenNameSpan = givenNameSpan[0]
+                nameField['given_name'] = givenNameSpan.select("text()").extract()[0]
+            familyNameSpan = nameSpan.select("span[@class='family-name']")
+            if familyNameSpan and len(familyNameSpan) == 1:
+                familyNameSpan = familyNameSpan[0]
+                nameField['family_name'] = familyNameSpan.select("text()").extract()[0]
+            personProfile['name'] = nameField
+        else:
+            return None
         ## Also view
         alsoViewProfileList = []
         divExtra = hxs.select("//div[@id='extra']")
@@ -22,13 +36,15 @@ class HtmlParser:
                         alsoViewItem = UnicodeDammit(alsoViewItem).markup
                         item = HtmlParser.get_also_view_item(alsoViewItem)
                         alsoViewProfileList.append(item)
+                    personProfile['also_view'] = alsoViewProfileList
         return personProfile
 
     @staticmethod
     def get_also_view_item(dirtyUrl):
         item = {}
-        item['linkedin_id'] = HtmlParser.remove_url_parameter(dirtyUrl)
-        item['url'] = HtmlParser.get_linkedin_id(dirtyUrl)
+        url = HtmlParser.remove_url_parameter(dirtyUrl)
+        item['linkedin_id'] = url 
+        item['url'] = HtmlParser.get_linkedin_id(url)
         return item
         
         
